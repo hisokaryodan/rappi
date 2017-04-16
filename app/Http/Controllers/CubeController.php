07bf6d9c33b146ab5511a29beacf15c071a7abd7
CamiloUrregoYaxa <camilo.urrego@yaxa.co>
2017-04-16 18:21:44 -0500

    solucion cubo

--- app/Http/Controllers/CubeController.php
+++ app/Http/Controllers/CubeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\VarDumper\VarDumper;
 
 class CubeController extends Controller {
@@ -23,6 +24,8 @@ class CubeController extends Controller {
 	 */
 	public function create() {
 		//
+//		echo "toma";
+
 		return view('cube/create');
 	}
 
@@ -41,8 +44,7 @@ class CubeController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function store(Request $request) {
-		//
-//		VarDumper::dump($request->all());
+
 		$this->validate($request, [
 			'valuex'        => 'required|integer|min:1|max:5',
 			'valuey'        => 'required|integer|min:1|max:5',
@@ -51,9 +53,21 @@ class CubeController extends Controller {
 
 		$cube_settings = $request->all();
 		unset($cube_settings['_token']);
-		$request->session()->put('cube_settings', $cube_settings);
-//		VarDumper::dump($request->session()->get('cube_settings'));
 
+		$request->session()->put('cube_settings', $cube_settings);
+
+		$x_axis = $request->session()->get('cube_settings.valuex');
+		$y_axis = $z_axis = $request->session()->get('cube_settings.valuey');
+
+		$cube = array_fill(0, $x_axis, array_fill(0, $y_axis, array_fill(0, $z_axis, 0)));
+
+		if ($request->session()->exists('cube')) {
+			$request->session()->remove('cube');
+		}
+
+		$request->session()->put('cube', $cube);
+
+		return view('cube/edit', ['repeat' => $request->session()->get('cube_settings.operationsqty')]);
 	}
 
 	/**
@@ -78,7 +92,7 @@ class CubeController extends Controller {
 	 */
 	public function edit($id) {
 		//
-		return view('cube/edit');
+		return view('cube/edit', ['repeat' => 2]);
 	}
 
 	/**
@@ -90,13 +104,68 @@ class CubeController extends Controller {
 	 * @return \Illuminate\Http\Response
 	 */
 	public function update(Request $request, $id = null) {
-		//
-		VarDumper::dump($request->all());
-		$cube = array();
-//		$request->session()->put('key', 'value');
-		$cube[$request->input('updatex')][$request->input('updatey')][$request->input('updatez')] = $request->input('updatew');
-		VarDumper::dump($cube);
-//		VarDumper::dump([$w,$x,$y,$w]);
+		$query_type = $request->get('query_type');
+
+		foreach ($request->get('query_string') as $key => $query_string) {
+
+			if ($query_type[ $key ] == 'UPDATE') {
+
+				$split           = str_split($query_string);
+				$slice_for_value = array_slice($split, 3);
+				$update_value    = implode('', $slice_for_value);
+				$cube            = $request->session()->get('cube');
+
+				$x = ($split[0] - 1);
+				$y = ($split[1] - 1);
+				$z = ($split[2] - 1);
+
+				$cube[ $x ][ $y ][ $z ] = $update_value;
+
+				$request->session()->put('cube', $cube);
+
+			} elseif ($query_type[ $key ] == 'QUERY') {
+
+				//Divido el arreglo en dos parte para establecer rangos desde y hasta
+				$split  = str_split($query_string);
+				$chunks = array_chunk($split, (count($split) / 2));
+				$from   = isset($chunks[0]) ? $chunks[0] : [];
+				$to     = isset($chunks[1]) ? $chunks[1] : [];
+
+
+				if ( ! isset($from[0]) || ! isset($from[1]) || ! isset($from[2]) || ! isset($to[0]) || ! isset($to[1]) || ! isset($to[2])) {
+					throw new BadRequestHttpException('Los valores de entrada no tienen el formato correcto');
+				} else {
+					// A cada eje se le resta 1 para coincidir con los índices del json
+					$from_axis_x = ($from[0] - 1);
+					$from_axis_y = ($from[1] - 1);
+					$from_axis_z = ($from[2] - 1);
+					// A cada eje se le resta 1 para coincidir con los índices del json
+					$to_axis_x = ($to[0] - 1);
+					$to_axis_y = ($to[1] - 1);
+					$to_axis_z = ($to[2] - 1);
+
+					$data      = $request->session()->get('cube');
+					$total_sum = 0;
+
+
+					// Desde el valor indicado al eje x hasta el valor indicado al eje x
+					for ($x = $from_axis_x; $x <= $to_axis_x; $x ++) {
+						// Desde el valor indicado al eje y hasta el valor indicado al eje y
+						for ($y = $from_axis_y; $y <= $to_axis_y; $y ++) {
+							// Desde el valor indicado al eje z hasta el valor indicado al eje z
+							for ($z = $from_axis_z; $z <= $to_axis_z; $z ++) {
+								//Se suma el valor del índice al total
+								$total_sum += isset($data[ $x ][ $y ][ $z ]) ? $data[ $x ][ $y ][ $z ] : 0;
+							}
+						}
+					}
+
+					VarDumper::dump($total_sum);
+
+					// return $total_sum;
+				}
+			}
+		}
 	}
 
 	/**
--- resources/views/cube/edit.blade.php
+++ resources/views/cube/edit.blade.php
@@ -1,72 +1,78 @@
 <!DOCTYPE html>
 <html lang="">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Title Page</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title Page</title>
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-			<![endif]-->
-		</head>
-		<body>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
 
-			<div class="row">
-				<div class="col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2">
-					<div class="page-header">
-						<h1>Cube summation<small> - Camilo Urrego</small></h1>
-					</div>
+<div class="row">
+    <div class="col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2">
+        <div class="page-header">
+            <h1>Cube summation
+                <small> - Camilo Urrego</small>
+            </h1>
+        </div>
 
-					<div class="row">
-						<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-							@if (count($errors) > 0)
-							<div class="alert alert-danger">
-								<ul>
-									@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-									@endforeach
-								</ul>
-							</div>
-							@endif
-							{{ Form::open(array('url' => 'cube/1', 'method' => 'patch', 'role' => 'form')) }}
-							<legend>Value Cube</legend>
+        <div class="row">
+            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
-							<div class="form-group">
-								{{ Form::label('updatex', 'value update X') }}
-								{{ Form::text('updatex', '', array('class' => 'form-control', 'placeholder' => 'value update X')) }}
-							</div>
-							<div class="form-group">
-								{{ Form::label('updatey', 'value update Y') }}
-								{{ Form::text('updatey', '', array('class' => 'form-control', 'placeholder' => 'value update Y')) }}
-							</div>
-							<div class="form-group">
-								{{ Form::label('updatez', 'value update Z') }}
-								{{ Form::text('updatez', '', array('class' => 'form-control', 'placeholder' => 'value update Z')) }}
-							</div>
-							<div class="form-group">
-								{{ Form::label('updatew', 'value update W') }}
-								{{ Form::text('updatew', '', array('class' => 'form-control', 'placeholder' => 'value update W')) }}
-							</div>
-							{{ Form::submit('Enviar', array('class' => 'btn btn-primary')) }}
-							
-							{{ Form::close() }}
-						</div>
-					</div>	
-				</div>
-			</div>
+                {{ Form::open(array('url' => 'cube/1', 'method' => 'patch', 'role' => 'form', 'class' => 'form-horizontal')) }}
+                <legend>Value Cube</legend>
+                @for( $i=0;$i < $repeat; $i++)
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+                                {{ Form::label('update', 'Seleccione la acción') }}
+                                {{ Form::select('query_type[]', [
+                                'QUERY' => 'QUERY',
+                                'UPDATE' => 'UPDATE'
+                                ], [$i], ['class' => 'form-control']) }}
+                            </div>
+                            <div class="col-xs-7 col-sm-7 col-md-7 col-lg-7">
+                                {{ Form::label('value', 'digite los valores') }}
+                                {{ Form::text('query_string[]', '', array('class' => 'form-control', 'placeholder' => 'value')) }}
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+                {{ Form::submit('Enviar', array('class' => 'btn btn-primary')) }}
 
-			<!-- jQuery -->
-			<script src="//code.jquery.com/jquery.js"></script>
-			<!-- Bootstrap JavaScript -->
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-			<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-			<script src="Hello World"></script>
-		</body>
-		</html>
+                {{ Form::close() }}
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- jQuery -->
+<script src="//code.jquery.com/jquery.js"></script>
+<!-- Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="Hello World"></script>
+</body>
+</html>
